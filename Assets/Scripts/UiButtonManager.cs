f156896e8affbb369fdc713fb05515da6f40508e
NepNath <115375139+NepNath@users.noreply.github.com>
2026-04-23 23:42:28 +0200

    0.1.0 - added TMP and started main menu scene and UI button script

---
+++ Assets/Scripts/UiButtonManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class UiButtonManager : MonoBehaviour
+{
+    void quitGame()
+    {
+        Application.Quit();
+    }
+
+    void startNewPlanet()
+    {
+        Debug.Log("Starting New Planet");
+    }
+
+    void openCollection()
+    {
+        Debug.Log("Opening Collection");
+    }
+    
+    
+    
+    
+}
